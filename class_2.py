9c1be8e3ba7e3ac782ebf22fb6382c618830131c
Sibtul Hassan <hassan0007267@gmail.com>
2024-08-06 20:48:49 +0500

    first commit

---
+++ class_2.py
@@ -0,0 +1,81 @@
+#Q 1
+name:str="my name is hassan ali"
+print(name) 
+#Q 2
+atom:str="this is my laptop"
+print (atom)
+atom:str="this is my cell phone"
+print(atom)
+#Q 3
+name:str="Qaid e Azam"
+message:str=(f"Once {name} said , Work work and work")
+print(message)
+#Q 4
+name:str="hassan mehar"
+print("Upper case = ", name.upper())
+print("lowe case =", name.lower())
+#Q 6
+quotename:str="Hazrat Ali"
+message:str=(f"once {quotename} said, \"this entire world is not worth are single tear\"")
+print(message)
+#Q 5
+famous_quote:str="\"The way to get started is to quit talking and begin doing. -Walt Disney\""
+print(famous_quote)
+#Q 7
+
+# work it white space
+spacing:str="\t\n hassan ali mehar\t\n"
+print("now using the remove space from left")
+print(name.rstrip())
+print(spacing.rstrip())
+print(spacing.strip())
+#Q 8
+X=5
+Y=10
+Z=15
+print(X+Y+Z)
+#Q 9
+a=10
+b=20
+a,b=b,a
+print("print the value of a",a)
+print("print the value of b",b)
+#Q 10
+favorite_colour="black"
+print("my favorite colour is",favorite_colour)
+new_favorite_colour="favorite_colour"
+print("my favorite colour is again still is",favorite_colour)
+#Q 11
+pet_name="buddy"
+print("the pet name is",pet_name)
+pet_name="max"
+print("the new pet name is",pet_name)     
+#Q 12
+name="sunshine"
+print("thie word is",name)
+#print("this name is",sunshine)
+#Q 13
+score=200
+print("the score is",score)
+score=150
+print("the updated score is",score)
+#Q 14
+city:str="bahawalpur"
+print("the city name is", city)
+#Q 15
+text="python programinig"
+print(text.title())
+#Q 16
+mix_case="ThiS iS tHE mIXx CaSE PROgRaminG"
+print(mix_case.lower())
+#Q 17
+mix_case="ThiS iS tHE mIXx CaSE PROgRaminG"
+print(mix_case.upper())
+#Q 18
+tempreture="25"
+print("today tempreture is {tempreture} degree" )
+#Q 19
+poem="'baby baby yes mama.\neating suger no mama.\ntelling a lie no mama.\nopen your mouth.'" 
+print(poem)
+
+
